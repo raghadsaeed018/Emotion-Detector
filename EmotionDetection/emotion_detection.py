ad5e5e7f9ee942eb2e9084c90c75ce25d9f39e2a
raghadsaeed018 <raghadsaeed018@gmail.com>
2024-10-25 07:51:46 -0400

    6th version, error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -10,14 +10,26 @@ def emotion_detector(text_to_analyse):
 
     fresponse = json.loads(response.text)
 
-    emotions = {
-        'anger': fresponse['emotionPredictions'][0]['emotion']['anger'],
-        'disgust': fresponse['emotionPredictions'][0]['emotion']['disgust'],
-        'fear': fresponse['emotionPredictions'][0]['emotion']['fear'],
-        'joy': fresponse['emotionPredictions'][0]['emotion']['joy'],
-        'sadness': fresponse['emotionPredictions'][0]['emotion']['sadness']
-    }
 
-    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    if response.status_code == 200:
+        emotions = {
+            'anger': fresponse['emotionPredictions'][0]['emotion']['anger'],
+            'disgust': fresponse['emotionPredictions'][0]['emotion']['disgust'],
+            'fear': fresponse['emotionPredictions'][0]['emotion']['fear'],
+            'joy': fresponse['emotionPredictions'][0]['emotion']['joy'],
+            'sadness': fresponse['emotionPredictions'][0]['emotion']['sadness']
+        }
+
+        emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+
+    elif response.status_code == 400 or response.status_code == 500:
+        emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
     return emotions
--- server.py
+++ server.py
@@ -9,6 +9,9 @@ def emo_detector():
     
     response = emotion_detector(text_to_analyze)
     
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    
     return f"For the given statement, the system response is 'anger': {response['anger']}, \
     'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} \
     and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
